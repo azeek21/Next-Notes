d6d0e436b71df1f1597e498a331149f0f3183f84
azee <pymanuz@gmail.com>
2023-02-07 20:09:33 +0300

    some play with api

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,48 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type Answer = {
+  id: number,
+  answer: string,
+}
+
+type Question = {
+  id: number,
+  question: string,
+  multiple_answers: boolean,
+  answers: Array<Answer>,
+  correct_answers?: number[] | null
+}
+
 type Data = {
-  name: string
+  questions: Array<Question>
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    {
+      questions: [
+        {
+          id: 1,
+          question: 'A question here',
+          multiple_answers: true,
+          answers : [
+            {
+              id: 1,
+              answer: 'Answer with id 1'
+            },
+            {
+              id: 2,
+              answer: 'Answer with id 2'
+            },
+          ],
+          correct_answers: [1,2]
+        }
+      ]
+    }
+  )
 }
+
