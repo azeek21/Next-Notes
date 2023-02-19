5c4daccd39355de4c068680d92070d92ca299a45
azee <pymanuz@gmail.com>
2023-02-19 15:44:38 +0300

    APIs: handling post request, unfinished

--- src/pages/comments/index.tsx
+++ src/pages/comments/index.tsx
@@ -1,15 +1,28 @@
 import { CommentType } from "@/data/comments";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 export default function CommetList() {
     const [data, setData] = useState([] as CommentType[])
     const [loading, setLoading] = useState(false);
+    const [comment, setComment] = useState({
+        id: data[-1]?.id + 1,
+        text: ''
+    })
 
+    
     const fetchComments =async () => {
         setLoading(true)
         const data: CommentType[] = await (await fetch('/api/v1/comments')).json();
         setLoading(false);
         setData(data);
+    }
+
+    const handleInput = (ev: ChangeEvent<HTMLInputElement>) => {
+        setComment(old => ({...old, text: ev.target.value}))
+    };
+
+    const submitComment = () => {
+        console.log("SUBMIT")
     }
 
     return (
@@ -18,6 +31,14 @@ export default function CommetList() {
             <button onClick={fetchComments} >Click to load comments</button>
             {loading && <h4>Loading...</h4>}
             {data && data.map(d => <h3 key={d.id} > <span>comment:</span> {d.text}</h3>)}
+            <form onSubmit={(ev) => {
+                ev.preventDefault();
+                submitComment();
+            }}>
+                <label htmlFor="comment">Enter new Comment</label>
+                <input id="comment" onChange={handleInput} type="text" value={comment?.text} autoComplete="false" />
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
